bc1247d0c20e24fa88256dc70e3b3998443d5671
Abdón Rodríguez Davila <a@abdonrd.com>
2017-10-12 17:09:18 +0200

    Update selector-list-comma-space-after rule

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ module.exports = {
     'selector-type-case': 'lower',
     'selector-list-comma-newline-after': 'always',
     'selector-list-comma-newline-before': 'never-multi-line',
-    'selector-list-comma-space-after': 'never',
+    'selector-list-comma-space-after': 'always-single-line',
     'selector-list-comma-space-before': 'never',
     'rule-empty-line-before': ['always', {
       'except': ['first-nested'],
